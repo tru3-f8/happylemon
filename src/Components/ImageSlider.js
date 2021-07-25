a679ca702ed6c0213f29b6840f9a2d5d3bc7b928
tru3-f8 <73862483+tru3-f8@users.noreply.github.com>
2021-07-24 22:39:53 -0700

    happy lemon

---
+++ src/Components/ImageSlider.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function ImageSlider() {
+    return (
+        <ImageSliderContainer>
+            
+        </ImageSliderContainer>
+    )
+}
+
+export default ImageSlider
+
+
+const ImageSliderContainer = styled.div`
+background: blue;
+height: 700px;
+width: 1200px;
+margin: 35px auto;
+
+`;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Navbar() {
+  return (
+    <NavbarContainer>
+      <NavbarListContainer>
+        <NavbarList>
+          <NavbarListItem>Menu</NavbarListItem>
+          <NavbarListItem>Location</NavbarListItem>
+          <NavbarListItem>Top 10</NavbarListItem>
+          <NavbarListItem>About</NavbarListItem>
+          <NavbarListItem>Contact</NavbarListItem>
+        </NavbarList>
+      </NavbarListContainer>
+    </NavbarContainer>
+  );
+}
+
+export default Navbar;
+
+const NavbarContainer = styled.div`
+  background: #000;
+  width: 100%;
+  height: 130px;
+`;
+
+const NavbarListContainer = styled.div`
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+
+  @media screen and (min-width: 768px) {
+      width: 550px;
+      margin: 0 auto;
+  }
+`;
+
+const NavbarList = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 100%;
+`;
+
+const NavbarListItem = styled.div`
+  color: #fff;
+`;
