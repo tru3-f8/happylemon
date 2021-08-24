9e093330c96723158635eb8453b08185782a79ce
tru3-f8 <73862483+tru3-f8@users.noreply.github.com>
2021-08-24 16:47:14 -0700

    happy lemon

--- src/Map/Mapbox.js
+++ src/Map/Mapbox.js
@@ -8,102 +8,72 @@ function Mapbox() {
   const mapboxId = process.env.REACT_APP_MAPBOX_ID;
 
   const mapContainer = useRef(null);
-  const map = useRef(null);
+  // const map = useRef(null);
   // const [lng, setLng] = useState(-70.9);
   // const [lat, setLat] = useState(42.35);
   // const [zoom, setZoom] = useState(9);
 
-  // const geojson = {
-  //   type: 'FeatureCollection',
-  //   features: [
-  //     {
-  //       type: 'Feature',
-  //       properties: {
-  //         message: 'Foo',
-  //         iconSize: [60, 60],
-  //       },
-  //       geometry: {
-  //         type: 'Point',
-  //         coordinates: [-66.324462, -16.024695],
-  //       },
-  //     },
-  //     {
-  //       type: 'Feature',
-  //       properties: {
-  //         message: 'Bar',
-  //         iconSize: [50, 50],
-  //       },
-  //       geometry: {
-  //         type: 'Point',
-  //         coordinates: [-61.21582, -15.971891],
-  //       },
-  //     },
-  //     {
-  //       type: 'Feature',
-  //       properties: {
-  //         message: 'Baz',
-  //         iconSize: [40, 40],
-  //       },
-  //       geometry: {
-  //         type: 'Point',
-  //         coordinates: [-63.292236, -18.281518],
-  //       },
-  //     },
-  //   ],
-  // };
+  const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        properties: {
+          message: 'Foo',
+          iconSize: [60, 60],
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: [-66.324462, -16.024695],
+        },
+      },
+      {
+        type: 'Feature',
+        properties: {
+          message: 'Bar',
+          iconSize: [50, 50],
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: [-61.21582, -15.971891],
+        },
+      },
+      {
+        type: 'Feature',
+        properties: {
+          message: 'Baz',
+          iconSize: [40, 40],
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: [-63.292236, -18.281518],
+        },
+      },
+    ],
+  };
 
   useEffect(() => {
-    if (map.current) return;
-    map.current = new mapboxgl.Map({
+    // if (map.current) return;
+    // map.current = new mapboxgl.Map({
+    //   container: mapContainer.current,
+    //   style: mapboxId,
+    //   center: [-117.077, 32.689],
+    //   zoom: 10,
+
+    const map = new mapboxgl.Map({
       container: mapContainer.current,
       style: mapboxId,
       center: [-117.077, 32.689],
       zoom: 10,
+
     });
+
+    const marker = new mapboxgl.Marker().setLngLat([-116.9621115658552, 32.621190888609654]).addTo(map);
+   
+  
+
   });
 
-  // map.on('load', () => {
-  //   // Load an image from an external URL.
-  //   map.loadImage(
-  //   'https://docs.mapbox.com/mapbox-gl-js/assets/cat.png',
-  //   (error, image) => {
-  //   if (error) throw error;
-     
-  //   // Add the image to the map style.
-  //   map.addImage('cat', image);
-     
-  //   // Add a data source containing one point feature.
-  //   map.addSource('point', {
-  //   'type': 'geojson',
-  //   'data': {
-  //   'type': 'FeatureCollection',
-  //   'features': [
-  //   {
-  //   'type': 'Feature',
-  //   'geometry': {
-  //   'type': 'Point',
-  //   'coordinates': [-77.4144, 25.0759]
-  //   }
-  //   }
-  //   ]
-  //   }
-  //   });
-     
-  //   // Add a layer to use the image to represent the data.
-  //   map.addLayer({
-  //   'id': 'points',
-  //   'type': 'symbol',
-  //   'source': 'point', // reference the data source
-  //   'layout': {
-  //   'icon-image': 'cat', // reference the image
-  //   'icon-size': 0.25
-  //   }
-  //   });
-  //   }
-  //   );
-  //   });
-
-  
 
   return (
     <MapboxContainer>
